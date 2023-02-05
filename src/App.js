9a7db94b2684eef6b85642d5068c39518b852876
Aela B <aelab@iMac-von-Aela.local>
2023-02-05 10:45:27 +0200

    smile.gr

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// // import pages
+// import Home from './pages/Home';
+// import Praxis from './pages/Praxis';
+// import Service from './pages/Service';
+// import Implants from './pages/Implants';
+// import Robotics from './pages/Robotics';
+// import Esthetics from './pages/Esthetics';
+// import Gallery from './pages/Gallery';
+// import Map from './pages/Map';
+
+// import components
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Appointment from './components/Appointment';
+import Services from './components/Services';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Hero />
+      <About />
+      <Appointment />
+      <Services />
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='Praxis' element={<Praxis />} />
+        <Route path='service' element={<Service />} />
+        <Route path='implants' element={<Implants />} />
+        <Route path='robotics' element={<Robotics />} />
+        <Route path='esthetics' element={<Esthetics />} />
+        <Route path='gallery' element={<Gallery />} />
+        <Route path='map' element={<Map />} />
+        {/* <Route path='SingleService/:id' element={<SingleService />} /> */}
+      {/* </Routes> */}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import logo from '../images/implantlogo.png';
+import { pageLinks, socialLinks } from '../data';
+// import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        {/* <!-- nav header --> */}
+        <div className='nav-header'>
+          <img
+            src={logo}
+            className='nav-logo'
+            alt=''
+            width='32px'
+            height='42px'
+          />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
+          </button>
+        </div>
+        <ul className='nav-links' id='nav-links'>
+          {/* <li className='nav-link'>
+            <Link to='/'>αρχική</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Praxis'>το ιατρείο</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Service'>υπηρεσίες</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Implants'>εμφυτεύματα</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Robotics'>ρομποτική</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Esthetics'>αισθητική</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Gallery'>photos</Link>
+          </li>
+          <li className='nav-link'>
+            <Link to='/Map'>χάρτης</Link>
+          </li> */}
+
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className='nav-link'>
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        {/* 
+        <ul className='nav-icons'>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='nav-icon'
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
